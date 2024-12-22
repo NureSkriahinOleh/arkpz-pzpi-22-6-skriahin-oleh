557654ae7ecb7bafed001fc8e61fe290f23c8861
NureSkriahinOleh <oleh.skriahin@nure.ua>
2024-12-22 21:43:23 +0200

    Delete Task2/arkpz-pzpi-22-6-skriahin-oleh-task2/serializers.py

--- Task2/arkpz-pzpi-22-6-skriahin-oleh-task2/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import IncidentReport
-
-class IncidentReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IncidentReport
-        fields = '__all__'
-        read_only_fields = ['created_at']
